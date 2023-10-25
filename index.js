fa63c6e879171ed37e19c59a3c0959b32a609a4e
matvey <matvey.karlov@inbox.ru>
2023-10-25 23:31:39 +0300

    testing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import querystring from 'querystring';
 import { MongoClient } from 'mongodb';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -14,13 +15,46 @@ try{
 }catch (e) {
     console.log(e);
 }
+const ques = client.db().collection('questions');
+
+async function getStat(){
+    let arr = await ques.find().toArray();
+    return JSON.stringify(arr)
+}
 
 const server = http.createServer( (req,res) => {
+    console.log(req.url)
     if (req.url == '/') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             res.writeHead(200, {"Content-type" : "text/html"});
             res.end(data);
         })
+    }
+    if (req.url == '/stat') {
+        fs.readFile(path.join(__dirname, 'stat.html'), (err, data) => {
+            res.writeHead(200, {"Content-type" : "text/html"});
+            res.end(data);
+        })
+    }
+    if (req.url === '/getStat') {
+        getStat().then( arr => {
+            res.writeHead(200, {"Content-type" : "text"});
+            res.end(arr);
+        } )
+    }
+    let parsedData;
+    if (req.method == 'POST') {
+        let data = '';
+        
+        req.on('data', chunk => {
+        data += chunk.toString();
+        });
+        req.on('end', () => {
+            parsedData = querystring.parse(data);
+            console.log(typeof parsedData)
+            console.log(parsedData)
+            ques.insertOne(parsedData)
+        } )
     }
 } );
 
