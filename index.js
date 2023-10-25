a57dc31be8631092a6d4dc53d2038cc9fec0eea6
matvey <matvey.karlov@inbox.ru>
2023-10-25 23:53:35 +0300

    mongo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function getStat(){
 
 const server = http.createServer( (req,res) => {
     console.log(req.url)
-    if (req.method == 'GET') {
+
     if (req.url == '/') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             res.writeHead(200, {"Content-type" : "text/html"});
@@ -42,14 +42,7 @@ const server = http.createServer( (req,res) => {
             res.writeHead(200, {"Content-type" : "text"});
             res.end(arr);
         } )
-    }
-    if (req.url == '/test') {
-        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-            res.writeHead(200, {"Content-type" : "text/html"});
-            res.end(data);
-        })
-    }
-    }      
+    }  
     let parsedData;
     if (req.method == 'POST') {
         let data = '';
